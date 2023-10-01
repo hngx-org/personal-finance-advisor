b8e3d68547222f582c9ea380bdad978a8bbb5bf1
MurimiCaroline <murimicaroline20@gmail.com>
2023-10-01 17:50:41 +0300

    payment page is 75% complete

--- lib/src/features/payments/payment_page.dart
+++ lib/src/features/payments/payment_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:personal_finance_advisor/src/core/constants/dimensions.dart';
+import 'package:personal_finance_advisor/src/general_widgets/Custom_Small_Text_Form.dart';
 import 'package:personal_finance_advisor/src/general_widgets/custom_container_text_field.dart';
+import 'package:personal_finance_advisor/src/general_widgets/custom_elevated_button.dart';
 import 'package:personal_finance_advisor/src/general_widgets/custom_image_view.dart';
 import 'package:personal_finance_advisor/src/general_widgets/spacing.dart';
 
@@ -13,10 +15,10 @@ class PaymentPage extends StatelessWidget {
       child: Scaffold(
         body: Container(
           decoration: BoxDecoration(
-            color: Colors.white24,
+            color: Color(0xFF3369FF),
           ),
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(Dimensions.medium),
             child: Column(
               children: [
                 Column(
@@ -57,10 +59,75 @@ class PaymentPage extends StatelessWidget {
                 ),
                 Spacing.smallHeight(),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text("Hi"),
-                   Text("Hey")],
+                    Column(
+                      children: [
+                        Text("Expiry Date"),
+                        SizedBox(
+                          width: 100.0, // width of the text form field
+                          height: 50.0, // height of the text form field
+                          child: CustomSmallTextField(
+                            maxLines: 1,
+                            maxLength: 4,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text("CVC"),
+                        SizedBox(
+                          width: 100.0, // width of the text form field
+                          height: 50.0, // height of the text form field
+                          child: CustomSmallTextField(
+                            maxLines: 1,
+                            maxLength: 3,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Container(
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [
+                          Text("Pin"),
+                          SizedBox(
+                            width: 100.0, // width of the text form field
+                            height: 50.0, // height of the text form field
+                            child: CustomSmallTextField(
+                              maxLines: 1,
+                              maxLength: 4,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: CustomElevatedButton(
+                    text: " Pay",
+                    width: MediaQuery.sizeOf(context).width * 0.4,
+                    height: MediaQuery.sizeOf(context).height * 0.05,
+                    onTap: () {},
+                    buttonStyle: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        Color(0xFF5200FF),
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              4.0), // border radius of 10 pixels
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
