88049e63f483f4e682a3bde0ee912f9d90eba1d0
Moyinoluwa Shabi <shabimoyinoluwa19@gmail.com>
2023-10-03 22:10:56 +0100

    add auth functionality
    - & some code refactor

--- lib/src/features/Splash/splash_screen.dart
+++ lib/src/features/Splash/splash_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:personal_finance_advisor/src/core/utils/theme/colors.dart';
-import 'package:personal_finance_advisor/src/features/Login/login_page.dart';
+
 import 'package:personal_finance_advisor/src/features/Splash/screen_ui.dart';
-import 'package:personal_finance_advisor/src/features/auth/screens/sign_up_screen.dart';
-import 'package:personal_finance_advisor/src/features/chat/page/chat_screens.dart';
+
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:personal_finance_advisor/src/general_widgets/custom_image_view.dart';
+import 'package:personal_finance_advisor/src/features/onboarding/first_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -15,7 +14,7 @@ class SplashScreen extends StatelessWidget {
     return AnimatedSplashScreen(
       backgroundColor: AppColors.primaryMainColor,
       splash: const ScreenUI(),
-      nextScreen: const SignUpScreen(),
+      nextScreen: const FirstScreen(),
     );
   }
 }
