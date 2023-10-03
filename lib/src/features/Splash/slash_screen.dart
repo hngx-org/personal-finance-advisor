817c283dc7c2c60a1cee156e2251df229dbb085c
JohnnyBoi03 <johnnyblogs01@gmail.com>
2023-10-03 19:46:51 +0100

    onboarding screen

--- lib/src/features/Splash/slash_screen.dart
+++ lib/src/features/Splash/slash_screen.dart
@@ -3,6 +3,8 @@ import 'package:personal_finance_advisor/src/features/Login/login_page.dart';
 import 'package:personal_finance_advisor/src/features/Splash/screen_ui.dart';
 import 'package:personal_finance_advisor/src/features/chat/page/chat_screens.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:personal_finance_advisor/src/features/onboarding/first_screen.dart';
+import 'package:personal_finance_advisor/src/features/onboarding/onboarding.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -11,7 +13,7 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return AnimatedSplashScreen(
       splash: const ScreenUI(),
-      nextScreen: const LoginPage(),
+      nextScreen: const FirstScreen(),
     );
   }
 }
---
+++ lib/src/features/onboarding/forth_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:personal_finance_advisor/src/features/Login/login_page.dart';
+
+class ForthScreen extends StatelessWidget {
+  const ForthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              //illustrator
+              SvgPicture.asset('lib/src/asset/onboarding4.svg'),
+
+              const SizedBox(
+                height: 60,
+              ),
+
+              //bold text
+              const Padding(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: Text('Intelligent Financial Insights',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF0E2C66)),
+                    textAlign: TextAlign.center),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+
+              //sub text
+              const Text(
+                  'Receive tailored advice and insights to help you make informed decisions for your financial well-being',
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'Nunito Sans'),
+                  textAlign: TextAlign.center),
+              // const SizedBox(
+              //   height: 69,
+              // ),
+              Spacer(),
+
+              //navigation in row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      '',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF0E2C66)),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginPage(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Next',
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF0E2C66)),
+                    ),
+                  ),
+                ],
+              ),
+              Spacer()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
